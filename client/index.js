2a4bbdb20bced7a2728741a6a65b405c16e936ac
Jesse Shawl <jesse@jesse.sh>
2024-09-14 19:40:47 -0500

    use dynamic apiUrl

--- client/index.js
+++ client/index.js
@@ -1,13 +1,13 @@
+const apiUrl = "http://localhost:8787";
+
 async function translate(key) {
-  const response = await fetch(
-    `http://localhost:8787/api/translate?key=${key}`
-  );
+  const response = await fetch(`${apiUrl}/api/translate?key=${key}`);
   const data = await response.json();
   return data.value;
 }
 
 async function debug() {
-  const response = await fetch(`http://localhost:8787/api/debug`);
+  const response = await fetch(`${apiUrl}/api/debug`);
   const data = await response.json();
   return data;
 }
