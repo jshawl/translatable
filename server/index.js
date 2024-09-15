a51dcd85c796a7c0aaab0bd0d228022930988876
Jesse Shawl <jesse@jesse.sh>
2024-09-14 19:01:56 -0500

    client + server

---
+++ server/index.js
@@ -0,0 +1,45 @@
+import { Hono } from "hono";
+const app = new Hono();
+
+const translations = {
+  en: {
+    Hello: "Hello",
+  },
+  fr: {
+    Hello: "Bonjour",
+  },
+};
+
+const locales = (acceptLanguage) =>
+  acceptLanguage
+    .split(/[q=\d\.\d]/)
+    .flatMap((locale) => locale.replace(";", "").split(","))
+    .filter(String);
+
+app.get("/api/debug", (c) => {
+  const acceptLanguage = c.req.header("Accept-Language");
+
+  return c.json({
+    "Accept-Language": acceptLanguage,
+    locales: locales(acceptLanguage),
+  });
+});
+
+app.get("/api/translate", (c) => {
+  const ls = locales(c.req.header("Accept-Language"));
+  const key = c.req.query("key");
+  for (let i = 0; i < ls.length; i++) {
+    const value = translations[ls[i]]?.[key];
+    if (value) {
+      return c.json({
+        value,
+      });
+    }
+  }
+  c.status(404);
+  return c.json({
+    error: "Not Found",
+  });
+});
+
+export default app;
